a7594658ba1f8c3d75f7d144cbac4c47e428798c
penguinears <95500180+penguinears@users.noreply.github.com>
2026-05-30 13:37:59 +0100

    Update and rename images/vault.env to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBzk5p9naKJZndFHfqpldxGUMLBGzkDIsU",
+  authDomain: "antianti-69313.firebaseapp.com",
+  databaseURL: "https://antianti-69313-default-rtdb.firebaseio.com",
+  projectId: "antianti-69313",
+  storageBucket: "antianti-69313.firebasestorage.app",
+  messagingSenderId: "147709968684",
+  appId: "1:147709968684:web:696108f146ecda9b5eec89",
+  measurementId: "G-WXRNC9P09F"
+};
